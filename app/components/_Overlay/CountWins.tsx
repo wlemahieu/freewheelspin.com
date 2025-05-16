60ff2d171320c7b80a67c8bf81b2c7d8e6cb0b84
Wesley LeMahieu <wesleylemahieu@gmail.com>
2025-05-16 15:24:26 -0700

    prevent checkbox change during spin

--- app/components/_Overlay/CountWins.tsx
+++ app/components/_Overlay/CountWins.tsx
@@ -1,13 +1,15 @@
-import { useConfigStore } from "../useStore";
+import { useConfigStore, useSpinnerStore } from "../useStore";
 
 export default function CountWins() {
   const { removeWinners, countWins, setCountWins } = useConfigStore();
+  const isSpinning = useSpinnerStore((s) => s.isSpinning);
+
   return (
     <span className="flex gap-x-2">
       <label htmlFor="count-wins">count wins</label>
       <input
         type="checkbox"
-        disabled={removeWinners}
+        disabled={removeWinners || isSpinning}
         id="count-wins"
         checked={countWins}
         onChange={() => setCountWins(!countWins)}
--- app/components/_Overlay/RemoveWinners.tsx
+++ app/components/_Overlay/RemoveWinners.tsx
@@ -1,12 +1,14 @@
-import { useConfigStore } from "../useStore";
+import { useConfigStore, useSpinnerStore } from "../useStore";
 
 export default function RemoveWinners() {
   const { removeWinners, setRemoveWinners } = useConfigStore();
+  const isSpinning = useSpinnerStore((s) => s.isSpinning);
   return (
     <span className="flex gap-x-2">
       <label htmlFor="remove-winners">remove winners</label>
       <input
         type="checkbox"
+        disabled={isSpinning}
         id="remove-winners"
         checked={removeWinners}
         onChange={() => setRemoveWinners(!removeWinners)}
