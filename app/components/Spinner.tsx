4826e9884db8d18b0577b1ff786f01dfa5fb1872
Wesley LeMahieu <wesleylemahieu@gmail.com>
2025-05-15 20:29:50 -0700

    remove spinner hitbox, use actual spinner mesh

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -5,7 +5,9 @@ import {
 } from "./useEffects";
 import SegmentSlice from "./_Spinner/SegmentSlice";
 import { useSpinnerStore } from "./useStore";
-import SpinnerHitbox from "./_Spinner/SpinnerHitbox";
+import { useState } from "react";
+import { useCursor } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 
 export default function Spinner() {
   useAnimateSpinningWheel();
@@ -13,13 +15,24 @@ export default function Spinner() {
   useElevateSelectedSlice();
   const slices = useSpinnerStore((s) => s.slices);
 
+  const isSpinning = useSpinnerStore((s) => s.isSpinning);
+  const { scene } = useThree();
+  const [hovered, setHovered] = useState(false);
+
+  const spinWheel = useSpinnerStore.getState().spinWheel;
+
+  useCursor(!isSpinning && hovered, "pointer", "auto", document.body);
+  useCursor(isSpinning && hovered, "not-allowed", "auto", document.body);
+
   return (
     <group
       ref={(el) => {
         useSpinnerStore.getState().spinnerRef = el;
       }}
+      onClick={() => spinWheel(scene)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
     >
-      <SpinnerHitbox />
       {slices.map((slice, index) => (
         <SegmentSlice key={slice.name} index={index} slice={slice} />
       ))}
