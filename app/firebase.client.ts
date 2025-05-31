05765f7160cb4c1faa84c975af483772a181ffce
Wesley LeMahieu <wesleylemahieu@gmail.com>
2025-05-31 14:32:34 -0700

    cleanup firebase client, export analytics

--- app/firebase.client.ts
+++ app/firebase.client.ts
@@ -1,20 +1,7 @@
 import { initializeApp } from "firebase/app";
-/*
-import {
-  connectAuthEmulator,
-  createUserWithEmailAndPassword,
-  getAuth,
-  inMemoryPersistence,
-  setPersistence,
-  signInWithEmailAndPassword,
-} from "firebase/auth";*/
-import {
-  connectFirestoreEmulator,
-  doc,
-  getFirestore,
-  setDoc,
-} from "firebase/firestore";
-import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions";
+import { getAnalytics } from "firebase/analytics";
 
 const app = initializeApp({
   apiKey: "AIzaSyAdHy_Y8yAEX5BSwQGSWVyoyOa-LEaARm4",
@@ -27,38 +14,10 @@ const app = initializeApp({
 });
 const region = "us-west1";
 
-// export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app, region);
-
-// setPersistence(auth, inMemoryPersistence);
-
-/*
-export const signUp = async (email: string, password: string) => {
-  const credential = await createUserWithEmailAndPassword(
-    auth,
-    email,
-    password
-  );
-  const { user } = credential;
-
-  await setDoc(doc(db, "users", user.uid), {
-    email: user.email,
-    createdAt: new Date(),
-  });
-
-  return user;
-};
-
-export const signIn = async (email: string, password: string) => {
-  const credential = await signInWithEmailAndPassword(auth, email, password);
-  const { user } = credential;
-  return user;
-};
-*/
+export const analytics = getAnalytics(app);
 
 if (process.env.NODE_ENV !== "production") {
   connectFirestoreEmulator(db, "127.0.0.1", 8086);
-  // connectAuthEmulator(auth, "http://localhost:9099");
-  // connectFunctionsEmulator(functions, "127.0.0.1", 5001);
 }
